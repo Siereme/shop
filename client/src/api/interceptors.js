54cda24847bcbcdbd76335fcac1412d5ec628f39
Siereme <sr.kotenev@gmail.com>
2022-08-04 15:45:04 +0400

    Configure refresh token processing on the client

---
+++ client/src/api/interceptors.js
@@ -0,0 +1,32 @@
+
+import api from './backend-api'
+import axiosApi from "./api"
+
+
+const setup = (store) => {
+    axiosApi.interceptors.request.use(
+      (config) => {
+        var accessToken = store.getters.getAccessToken()
+        if (accessToken && !config.url.includes('/refresh')) {
+          config.headers['Access_Authorization'] = accessToken
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+    axiosApi.interceptors.response.use((response) => {
+        return response
+      }, async function (error) {
+        const originalRequest = error.config;
+        if (error.response?.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+          await Promise.resolve(api.refreshAccessToken())          
+          return axiosApi(originalRequest);
+        }
+        return Promise.reject(error);
+    });
+  };
+
+export default setup
